f71d2b07ce0d7c09bb1a10ec0c1989bb06bd6e88
Guilherme Palma <gomes.lmc@gmail.com>
2020-09-14 21:27:36 -0300

    Add global css

--- src/App.tsx
+++ src/App.tsx
@@ -1,5 +1,10 @@
 import React from 'react';
+import GlobalStyles from './styles/global';
 
-const App: React.FC = () => <h1>Hello World</h1>; 
-
+const App: React.FC = () => (
+  <>
+    <h1>Hello World</h1>
+    <GlobalStyles />
+  </>
+);
 export default App;
