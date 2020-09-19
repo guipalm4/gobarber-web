10cda1e75daceb50ebc5b4c912f46f08f30f1af0
Guilherme Palma <gomes.lmc@gmail.com>
2020-09-19 18:22:08 -0300

    Add Yup dependency for validate forms

--- src/pages/SignUp/index.tsx
+++ src/pages/SignUp/index.tsx
@@ -1,15 +1,29 @@
-import React from 'react';
+/* eslint-disable @typescript-eslint/ban-types */
+import React, { useCallback } from 'react';
 import { FiArrowLeft, FiLock, FiUser, FiMail } from 'react-icons/fi';
 import { Form } from '@unform/web';
+import * as Yup from 'yup';
 import { Container, Content, Background } from './styles';
 import logoImg from '../../assets/logo.svg';
 import Button from '../../components/Button';
 import Input from '../../components/Input';
 
 const SignIn: React.FC = () => {
-  function handleSubmit(data: object): void {
-    console.log(data);
-  }
+  const handleSubmit = useCallback(async (data: object) => {
+    try {
+      const schema = Yup.object().shape({
+        name: Yup.string().required('Nome obrigatório'),
+        email: Yup.string()
+          .required('E-mail obrigatório')
+          .email('Digite um e-mail válido'),
+        password: Yup.string().min(6, 'No mínimo 6 digitos'),
+      });
+
+      await schema.validate(data, { abortEarly: false });
+    } catch (err) {
+      console.log(err);
+    }
+  }, []);
 
   return (
     <Container>
