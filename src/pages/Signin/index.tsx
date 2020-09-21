43ecdd23931e8fe6fae08acef96a91adb3366200
Guilherme Palma <gomes.lmc@gmail.com>
2020-09-21 20:28:23 -0300

    Add hook useAuth

--- src/pages/Signin/index.tsx
+++ src/pages/Signin/index.tsx
@@ -1,11 +1,10 @@
-/* eslint-disable @typescript-eslint/ban-types */
-import React, { useCallback, useRef, useContext } from 'react';
+import React, { useCallback, useRef } from 'react';
 import { FiLogIn, FiLock, FiMail } from 'react-icons/fi';
 import { Form } from '@unform/web';
 import { FormHandles } from '@unform/core';
 import * as Yup from 'yup';
 import { Container, Content, Background } from './styles';
-import { AuthContext } from '../../context/AuthContext';
+import { useAuth } from '../../context/AuthContext';
 import logoImg from '../../assets/logo.svg';
 import Button from '../../components/Button';
 import Input from '../../components/Input';
@@ -19,7 +18,7 @@ interface SignInFormData {
 const SignIn: React.FC = () => {
   const formRef = useRef<FormHandles>(null);
 
-  const { user, signIn } = useContext(AuthContext);
+  const { signIn } = useAuth();
 
   const handleSubmit = useCallback(
     async (data: SignInFormData) => {
